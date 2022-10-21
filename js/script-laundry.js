b5d8a380da59a7617ccd962680a0c6b1d5389f24
nsoktab <nataliasokolowskaa@gmail.com>
2022-10-21 12:30:32 +0200

    working on a bar and timer

---
+++ js/script-laundry.js
@@ -0,0 +1,20 @@
+const startingMinutes = 10;
+let time = startingMinutes * 60;
+
+const countdownEl = document.getElementById("countdown");
+
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+  const minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  if (startingMinutes < 0) {
+    countdownEl.innerHTML = `Free`;
+  } else {
+    countdownEl.innerHTML = `${minutes}:${seconds}`;
+    time--;
+  }
+}
