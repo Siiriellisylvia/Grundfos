cb7f8bc931f90d81be0a051e1cc52d9696e81316
nsoktab <nataliasokolowskaa@gmail.com>
2022-10-22 22:08:14 +0200

    working on graph

--- js/script-laundry.js
+++ js/script-laundry.js
@@ -30,7 +30,7 @@ let graphContainer = new chart(graph, {
         label: "My First Dataset",
         data: [65, 59, 80, 81, 56, 55, 40],
         fill: false,
-        borderColor: "rgb(240, 192, 192)",
+        borderColor: "red",
         tension: 0.1,
       },
     ],
