5565085b3087481060242314e188336979f18fff
nsoktab <nataliasokolowskaa@gmail.com>
2022-10-26 10:27:53 +0200

    2323

--- js/script-facilities.js
+++ js/script-facilities.js
@@ -3,7 +3,13 @@ function changeImage(fileName) {
   clickCounter++;
   let img = document.querySelector("#original-layout");
   img.setAttribute("src", fileName);
+
+  let opacityChange = document.getElementsByClassName(".room-button");
+  opacityChange.style.opacity = "0.75";
   if (clickCounter == 2) {
     img = document.querySelector("#original-layout");
   }
+}
+function toggle() {
+  var x = document.getElementById;
 }
