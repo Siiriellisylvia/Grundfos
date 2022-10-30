b208852036ce4a4ab7184235c29de397edb5db63
Clairebis <113165797+Clairebis@users.noreply.github.com>
2022-10-30 23:03:07 +0100

    12

--- js/script-news.js
+++ js/script-news.js
@@ -9,12 +9,16 @@ elem.style.display = 'none';
 
 document.getElementById("maintenanceFilterButton").addEventListener("click", maintenanceFunction);
 function maintenanceFunction() {
-    maintenanceFilterButton.style.color = "#eba10c";  //change colour of icon ***only changing text atm ***need to look at toggle / off when others are clicked & when it's clicked twice
+    maintenanceFilterButton.style.color = "#eba10c";  //change colour of icon wording
+    communityFilterButton.style.color = "#000000";
+    laundryFilterButton.style.color = "#000000";
+    outdoorFilterButton.style.color = "#000000";
+    holidaysFilterButton.style.color = "#000000";
 }
 
 document.getElementById("maintenanceFilterButton").addEventListener("click", showFunction);
 function showFunction() {
-    elem.style.display = "block"; //show clear filter options
+    elem.style.display = "block"; //show clear filter option
 }
 
 //***display only maintenance items
@@ -23,10 +27,74 @@ function showFunction() {
 
 document.getElementById("communityFilterButton").addEventListener("click", communityFunction);
 function communityFunction() {
-    communityFilterButton.style.color = "#eba10c"; //change colour of icon - only changing text atm***
-}
+    communityFilterButton.style.color = "#eba10c"; //change colour of icon wording
+    maintenanceFilterButton.style.color = "#000000"; 
+    laundryFilterButton.style.color = "#000000";
+    outdoorFilterButton.style.color = "#000000";
+    holidaysFilterButton.style.color = "#000000";}
+
 
 document.getElementById("communityFilterButton").addEventListener("click", showFunction);
 function showFunction() {
-    elem.style.display = "block"; //show clear filter options
+    elem.style.display = "block"; //show clear filter option
 }
+
+
+/*click laundry filter*/
+document.getElementById("laundryFilterButton").addEventListener("click", laundryFunction);
+function laundryFunction() {
+    laundryFilterButton.style.color = "#eba10c"; //change colour of icon wording
+    maintenanceFilterButton.style.color = "#000000"; 
+    communityFilterButton.style.color = "#000000";
+    outdoorFilterButton.style.color = "#000000";
+    holidaysFilterButton.style.color = "#000000";
+}
+
+
+document.getElementById("laundryFilterButton").addEventListener("click", showFunction);
+function showFunction() {
+    elem.style.display = "block"; //show clear filter option
+}
+
+/*click outdoor filter*/
+document.getElementById("outdoorFilterButton").addEventListener("click", outdoorFunction);
+function outdoorFunction() {
+    outdoorFilterButton.style.color = "#eba10c"; //change colour of icon wording
+    maintenanceFilterButton.style.color = "#000000"; 
+    communityFilterButton.style.color = "#000000";
+    laundryFilterButton.style.color = "#000000";
+    holidaysFilterButton.style.color = "#000000";
+}
+
+
+document.getElementById("outdoorFilterButton").addEventListener("click", showFunction);
+function showFunction() {
+    elem.style.display = "block"; //show clear filter option
+}
+
+/*click holidays filter*/
+document.getElementById("holidaysFilterButton").addEventListener("click", holidaysFunction);
+function holidaysFunction() {
+    holidaysFilterButton.style.color = "#eba10c"; //change colour of icon wording
+    maintenanceFilterButton.style.color = "#000000"; 
+    communityFilterButton.style.color = "#000000";
+    laundryFilterButton.style.color = "#000000";
+    outdoorFilterButton.style.color = "#000000";
+}
+
+
+document.getElementById("holidaysFilterButton").addEventListener("click", showFunction);
+function showFunction() {
+    elem.style.display = "block"; //show clear filter option
+}
+
+/*click clear filter*/
+document.getElementById("clearFilterButton").addEventListener("click", clearFilterFunction);
+function clearFilterFunction() {
+    maintenanceFilterButton.style.color = "#000000"; //change colour of icon wording
+    communityFilterButton.style.color = "#000000";
+    laundryFilterButton.style.color = "#000000";
+    outdoorFilterButton.style.color = "#000000";
+    holidaysFilterButton.style.color = "#000000";
+}
+
