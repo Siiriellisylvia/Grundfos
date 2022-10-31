98349eb9680fef00da5b7a2d20971afc7b9307e3
Clairebis <113165797+Clairebis@users.noreply.github.com>
2022-10-31 11:03:02 +0100

    13

--- js/script-events.js
+++ js/script-events.js
@@ -1,2 +1,16 @@
 /* Events JS*/
 
+function myPopUp1() {
+    var myPopUp1 = window.open("events-popup1.html", "", "width=700,height=400,top=200,left=550");
+    myPopUp1.focus();
+  }
+
+  function myPopUp2() {
+    var myPopUp2 = window.open("events-popup2.html", "", "width=700,height=400,top=200,left=550");
+    myPopUp1.focus();
+  }
+
+  function myPopUp3() {
+    var myPopUp3 = window.open("events-popup3.html", "", "width=700,height=400,top=200,left=550");
+    myPopUp1.focus();
+  }
