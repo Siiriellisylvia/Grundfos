0e1c9046b77220acbcdc6ff578ba25d345a24719
yanitsatsoneva <113165735+yanitsatsoneva@users.noreply.github.com>
2022-11-01 13:36:13 +0100

    cf-eye

---
+++ js/script-contact-form.js
@@ -0,0 +1,8 @@
+const passwordField = document.querySelector("#password");
+const eyeIcon= document.querySelector("#eye");
+
+eye.addEventListener("click", function(){
+    this.classList.toggle("fa-eye-slash");
+    const type = passwordField.getAttribute("type") === "password" ? "text" : "password";
+    passwordField.setAttribute("type", type);
+  })
