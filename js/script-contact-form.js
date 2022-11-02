024f06764481eda6a2f558ebb2ac2e2e8f4cec1f
nsoktab <nataliasokolowskaa@gmail.com>
2022-11-02 13:21:07 +0100

    changes

--- js/script-contact-form.js
+++ js/script-contact-form.js
@@ -1,8 +1,9 @@
 const passwordField = document.querySelector("#password");
-const eyeIcon= document.querySelector("#eye");
+const eyeIcon = document.querySelector("#eye");
 
-eye.addEventListener("click", function(){
-    this.classList.toggle("fa-eye-slash");
-    const type = passwordField.getAttribute("type") === "password" ? "text" : "password";
-    passwordField.setAttribute("type", type);
-  })
+eye.addEventListener("click", function () {
+  this.classList.toggle("fa-eye-slash");
+  const type =
+    passwordField.getAttribute("type") === "password" ? "text" : "password";
+  passwordField.setAttribute("type", type);
+});
