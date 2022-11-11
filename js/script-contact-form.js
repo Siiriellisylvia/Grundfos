9c81ec115a64bf664de1bab47b7f366bbbf50cc1
yanitsatsoneva <113165735+yanitsatsoneva@users.noreply.github.com>
2022-11-11 09:26:48 +0100

    new

--- js/script-contact-form.js
+++ js/script-contact-form.js
@@ -1,31 +1,3 @@
-/*const passwordField = document.querySelector("#password");
-const eyeIcon = document.querySelector("#eye");
-
-eye.addEventListener("click", function(){
-    this.classList.toggle("fa-eye-slash");
-    const type = passwordField.getAttribute("type") === "password" ? "text" : "password";
-    passwordField.setAttribute("type", type);
-  })
-
-  const toggleModal = () => {
-    const { classList } = document.body;
-    if (classList.contains("open")) {
-      classList.remove("open");
-      classList.add("closed");
-    } else {
-      classList.remove("closed");
-      classList.add("open");
-    }
-  };
-
-
-eye.addEventListener("click", function () {
-  this.classList.toggle("fa-eye-slash");
-  const type =
-    passwordField.getAttribute("type") === "password" ? "text" : "password";
-  passwordField.setAttribute("type", type);
-});*/
-
 const passwordField = document.querySelector("#password");
 const eyeIcon = document.querySelector("#eye");
 
@@ -35,7 +7,7 @@ eye.addEventListener("click", function(){
     passwordField.setAttribute("type", type);
   })
 
-  const toggleModal = () => {
+  /*const toggleModal = () => {
     const { classList } = document.body;
     if (classList.contains("open")) {
       classList.remove("open");
@@ -56,17 +28,5 @@ eye.addEventListener("click", function () {
 
 eye.addEventListener("click", function () {
   passwordField.setAttribute("type", type);
-});
-
-
-/** actual form drop down menu*/
-function show(a){
-  document.querySelector('.text02').value = a;
-}
-
-let dropdown = document.querySelector('.dropdown');
-dropdown.onclick = function(){
-  dropdown.classList.toggle('active');
-}
-
+});*/
 
