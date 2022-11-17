cce075247e8d11aae955b5bdf311760501f00e6b
Siiriellisylvia <113165765+Siiriellisylvia@users.noreply.github.com>
2022-11-17 13:30:08 +0100

    small edits

--- js/script-404.js
+++ js/script-404.js
@@ -1,8 +1,39 @@
+//Date
+var dateToday = new Date();
+var dd = dateToday.getDate();
+var mm = dateToday.getMonth()+1; 
+var yyyy = dateToday.getFullYear();
+if(dd<10) 
+{
+    dd='0'+dd;
+} 
 
-var date = new Date();
-var current_date = date.getDate()+"."+(date.getMonth()+1)+"."+date.getFullYear();
-document.getElementById("date").innerHTML = current_date;
+if(mm<10) 
+{
+    mm='0'+mm;
+} 
+dateToday = dd+'.'+mm+'.'+yyyy;
+console.log(dateToday);
+document.getElementById("date").innerHTML = dateToday;
 
-var time = new Date();
-var current_time = date.getHours()+":"+(date.getMinutes());
-document.getElementById("time").innerHTML = current_time;
+
+//Time
+function time() {
+var timeToday = new Date();
+var hh = timeToday.getHours();
+var mm = timeToday.getMinutes(); 
+if(hh<10) 
+{
+    hh='0'+hh;
+} 
+
+if(mm<10) 
+{
+    mm='0'+mm;
+} 
+timeToday = hh+':'+mm;
+console.log(timeToday);
+document.getElementById("time").innerHTML = timeToday;
+}
+
+setInterval(time, 1000);
